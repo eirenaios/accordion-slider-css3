cc2be490c7cab235f0931872b64e4ebfa9272487
eirenaios <suresh.onezero@gmail.com>
2015-02-11 17:16:17 +0530

    Initial Push with code

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hover Slider</title>
+        <link href="assets/style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        
+        <div class="meta">
+            <h1>Accordion Hover Slider using CSS3</h1>   
+        </div>
+        
+        <div class="hover-slider">
+            <ul>
+                <li><a href="#"><img src="assets/image1.jpg" alt="slider"></a><h3>Slide1</h3></li>
+                <li><a href="#"><img src="assets/image2.jpg" alt="slider"></a><h3>Slide2</h3></li>
+                <li><a href="#"><img src="assets/image3.jpg" alt="slider"></a><h3>Slide3</h3></li>
+                <li><a href="#"><img src="assets/image4.jpg" alt="slider"></a><h3>Slide4</h3></li>
+                <li><a href="#"><img src="assets/image5.jpg" alt="slider"></a><h3>Slide5</h3></li>
+            </ul>
+        </div>
+        
+        <div class="meta">
+            <h4><a href="https://github.com/eirenaios/accordion-slider-css3">Download - Github</a></h4>
+            <h5><a href="http://sureshkaria.com">By: Eirenaios</a></h5>
+        </div>
+    </body>
+</html>
